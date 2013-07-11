1fddd576b839891eb323f32e57b0314ed3e36c61
Stephen Crawley <crawley.stephen@gmail.com>
2013-07-11 11:02:19 +1000

    Set the timestamps when creating Dataset_File objects (+ file headers)

--- atom_ingest.py
+++ atom_ingest.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2013, Centre for Microscopy and Microanalysis
+#   (University of Queensland, Australia)
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    *  Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#    *  Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#    *  Neither the name of the VeRSI, the VeRSI Consortium members, nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
 import feedparser
 import iso8601
 from posixpath import basename
@@ -10,6 +39,7 @@ from tardis.tardis_portal.models import Dataset, DatasetParameter, \
 from django.db import transaction
 from django.conf import settings
 import urllib2
+import datetime
 
 # Ensure filters are loaded
 try:
@@ -123,7 +153,9 @@ class AtomPersister:
 
     def process_enclosure(self, dataset, enclosure):
         filename = getattr(enclosure, 'title', basename(enclosure.href))
-        datafile = Dataset_File(filename=filename, dataset=dataset)
+        now = datetime.datetime.now()
+        datafile = Dataset_File(filename=filename, dataset=dataset,
+                                created_time=now, modification_time=now)
         try:
             datafile.mimetype = enclosure.mime
         except AttributeError:
